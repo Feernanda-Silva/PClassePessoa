cd494f0bbbdbf72775ecef558526792214d319ac
Fernanda Silva <fernanda.ds@live.com>
2022-08-24 00:07:51 -0300

    Add project files.

---
+++ PClassePessoa/Pessoa.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PClassePessoa
+{
+    internal class Pessoa
+    {
+        String nome;
+        String nascimento;
+        String cpf;
+        String telefone;
+
+        public override string ToString() //override: aproveita um metodo que ja existe
+        {
+            return "Nome: " + nome + "\nNascimento: " + nascimento + "\nCpf: " + cpf + "\nTelefone:" + telefone;
+        }
+        public void setNome(String nome)
+        {
+            this.nome = nome;
+        }
+
+        public void setNascimento(String nascimento)
+        {
+            this.nascimento = nascimento;
+        }
+
+        public void setCpf(String cpf)
+        {
+            this.cpf = cpf;
+        }
+
+        public void setTelefone(String telefone)
+        {
+            this.telefone = telefone;
+        }
+
+        public String getNome()
+        {
+            return nome;
+        }
+
+        public String getNascimento()
+        {
+            return nascimento;
+        }
+
+        public String getCpf()
+        {
+            return cpf;
+        }
+
+        public String getTelefone()
+        {
+            return telefone;
+        }
+    }
+}
---
+++ PClassePessoa/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PClassePessoa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa[] Pessoa = new Pessoa[3];
+
+            for (int i = 0; i < Pessoa.Length; i++)
+            {
+                Pessoa[i] = new Pessoa();
+                Console.WriteLine("Digite o nome da " + (i + 1) + " pessoa ");
+                Pessoa[i].setNome(Console.ReadLine());
+                Console.WriteLine("Digite a data de nascimento da " + (i + 1) + " pessoa: ");
+                Pessoa[i].setNascimento(Console.ReadLine());
+                Console.WriteLine("Digite o cpf da" + (i + 1) + " pessoa: ");
+                Pessoa[i].setCpf(Console.ReadLine());
+                Console.WriteLine("Digite o telefone da " + (i + 1) + " pessoa: ");
+                Pessoa[i].setTelefone(Console.ReadLine());
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < Pessoa.Length; i++)
+            {
+                Console.WriteLine("\nPessoa " + (i + 1) + "\n" + Pessoa[i].ToString());
+            }
+        }
+    }
+}
